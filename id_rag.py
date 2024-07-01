3d11315f0f635a529dac084b620aaa64c7331244
ins-tukumin <ins.tukumin@gmail.com>
2024-07-01 09:55:51 +0900

    new

--- id_rag.py
+++ id_rag.py
@@ -33,13 +33,13 @@ now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
 template = """
     私の日記情報を添付します。
     この日記を読んで、私の事をよく理解した上で会話してください。
-    そして、私の相談に乗って少しでも私の気持ちを楽にしてほしいです。
-    表面的なコミュニケーションではなく、私のことをよく理解した上で会話してほしいです。
-    受け身にならずに積極的に私に語り掛けてほしいです。
+    この会話では私のお悩み相談に乗ってほしいです。悩みは進路関係に関するものです。
+    必要に応じて、私の日記に書かれている情報を参照して、私の事を理解して会話してください。
+    ただ、”あなたの日記を読んでみると”といったような、日記を読んだ動作を直接示すような言葉は出力に含めないでください。
     敬語は使わないでください。私の友達になったつもりで砕けた口調で話してください。
     150~200字程度で話してください。
     日本語で話してください。
-    Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question. また、ドキュメントを提供しますので、適宜参照して会話してください。なお、日本語で話してください。:
+    Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question. :
     ------
     <ctx>
     {context}
@@ -74,7 +74,7 @@ select_temperature = 0.0
 
 if student_id:
     if not firebase_admin._apps:
-        cred = credentials.Certificate('ragtest-5e402-firebase-adminsdk-qz4o3-ce2b4f2ed6.json') 
+        cred = credentials.Certificate('rag-course-84ec8-firebase-adminsdk-ftxi5-e847a47ba1.json') 
         default_app = firebase_admin.initialize_app(cred)
     db = firestore.client()
     
